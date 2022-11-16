7cb0a19e084a686148f27bc1322ef17f4caaec44
dmay2020 <dmay2020@my.fit.edu>
2022-11-16 16:45:13 -0500

    Implemented basic recipe book functionality.

--- recipeBook/RecipeBook.h
+++ recipeBook/RecipeBook.h
@@ -1,7 +1,7 @@
 //
 //     RecipeBook.h
 //     RecipeBook
-//     Copyright © 2021 Derek May. All rights reserved.
+//     Copyright © 2022 Derek May. All rights reserved.
 //
 
 #include <iostream>
@@ -9,9 +9,17 @@
 #include <vector>
 #include <string>
 
+
 class recipe {
 public:
-	recipe(std::string title_ = "Title goes here.", std::string description_ = "Write a decription.") : title(title_), description(description_) {}
+	recipe(
+		std::string title_ = "Title goes here.",
+		std::string description_ = "Write a decription.",
+		std::vector<std::string> ingredient_ = { "NULL" },
+		std::vector<std::string> measurement_ = { "NULL" },
+		std::vector<std::string> instruction_ = { "NULL" }
+	)
+		: title(title_), description(description_), ingredient(ingredient_), measurement(measurement_), instruction(instruction_) {}
 
 	friend std::ostream& operator<<(std::ostream& os, const recipe& obj) {
 		os << obj.title << "\n" << obj.description << "\n\nIngredients:\n";
@@ -48,12 +56,15 @@ public:
 		return instruction.at(index);
 	} inline std::string getStep(int index) { return getInstruction(index); }
 
-	recipe addIngredient(std::string measurement_ = "NULL", std::string ingredient_ = "NULL") {
+	recipe addIngredient(std::string measurement_ = "[Placeholder]", std::string ingredient_ = "[Placeholder]") {
+		if (measurement[0] == "NULL") { measurement.pop_back(); }
+		if (ingredient[0] == "NULL") { ingredient.pop_back(); }
 		measurement.push_back(measurement_);
 		ingredient.push_back(ingredient_);
 		return *this;
 	}
-	recipe addInstruction(std::string instruction_ = "No instruction given.") {
+	recipe addInstruction(std::string instruction_ = "[Placeholder]") {
+		if (instruction[0] == "NULL") { instruction.pop_back(); }
 		instruction.push_back(instruction_);
 		return *this;
 	} inline recipe addStep(std::string argz) { addInstruction(argz); return *this; }
@@ -65,4 +76,30 @@ private:
 	std::vector<std::string> ingredient;
 	std::vector<std::string> measurement;
 	std::vector<std::string> instruction;
+};
+
+
+class recipeBook {
+public:
+	recipeBook(
+				std::string bookTitle_,
+				std::string bookDescription_
+			  )
+			  : bookTitle(bookTitle_), bookDescription(bookDescription_) {}
+
+	recipe getRecipe(unsigned int index) { return recipes[index]; }
+
+	std::string getTitle() { return bookTitle; }
+
+	std::string getDescription() { return bookDescription; }
+
+	recipeBook addRecipe(recipe input) {
+		recipes.push_back(input);
+		return *this;
+	}
+
+private:
+	std::string bookTitle;
+	std::string bookDescription;
+	std::vector<recipe> recipes;
 };
--- recipeBook/Source.cpp
+++ recipeBook/Source.cpp
@@ -1,9 +1,90 @@
+//
+//     Source.cpp
+//     RecipeBook Source
+//     Copyright © 2022 Derek May. All rights reserved.
+//
+
 #include <iostream>
 #include "RecipeBook.h"
 using namespace std;
 
+vector<vector<string>> inputMeasurementsAndIngredients() {
+	vector<vector<string>> measurementsAndIngredients;
+	vector<string> ingredient;
+	vector<string> measurement;
+	bool loop = true;
+	while (loop) {
+		string input;
+		cout << "    Enter the measurement for this ingredient (or Type X to quit): ";
+		std::getline(std::cin, input);
+		if (input == "X") {
+			loop = false;
+		}
+		else {
+			measurement.push_back(input);
+			input = "NULL";
+			cout << "    Enter the name of this ingredient (or Type X to cancel and quit): ";
+			std::getline(std::cin, input);
+			if (input == "X") {
+				measurement.pop_back();
+				loop = false;
+			}
+			else {
+				ingredient.push_back(input);
+				cout << "    Ingredient added. Total ingredients: [" << ingredient.size() << "]\n";
+			}
+		}
+	}
+	measurementsAndIngredients.push_back(measurement);
+	measurementsAndIngredients.push_back(ingredient);
+
+	return measurementsAndIngredients;
+}
+
+vector<string> inputInstructions() {
+	vector<string> instructions;
+	bool loop = true;
+	while (loop) {
+		string input = "NULL";
+		cout << "    Enter instruction #" << instructions.size()+1 << " (or Type X to quit): ";
+		std::getline(std::cin, input);
+		if (input == "X") {
+			loop = false;
+		}
+		else {
+			instructions.push_back(input);
+			cout << "    Instruction added. Total instructions: [" << instructions.size() << "]\n";
+		}
+	}
+	return instructions;
+}
+
+recipeBook newRecipe(recipeBook rb) {
+	cout << "Write a title for this new recipe: ";
+	string title = "NULL";
+	std::getline(std::cin, title);
+	cout << "Now, write a short description: ";
+	string description = "NULL";
+	std::getline(std::cin, description);
+	recipe r(title, description);
+	cout << "Now, list the ingredients you will need and their measurements:\n";
+	vector<vector<string>> measurementsAndIngredients = inputMeasurementsAndIngredients();
+	cout << "Now, list the steps needed to make this:\n";
+	vector<string> instruction = inputInstructions();
+
+	for (int i = 0; i < measurementsAndIngredients[1].size(); i++) {
+		r.addIngredient(measurementsAndIngredients[0][i], measurementsAndIngredients[1][i]);
+	}
+	for (int i = 0; i < instruction.size(); i++) {
+		r.addInstruction(instruction[i]);
+	}
+
+	rb.addRecipe(r);
+	return rb;
+}
+	
 int main() {
-	recipe a("Chicken Stew", "Grandma's best recipe ever!");
+	recipe a("Chicken Stew", "Everyone's favorite stew!");
 
 	a.addIngredient("1 tbsp", "Butter");
 	a.addIngredient("1 qt", "Chicken Broth");
@@ -13,7 +94,15 @@ int main() {
 	a.addInstruction("Add to pot with broth and chicken.");
 	a.addInstruction("Bring to a simmer and serve.");
 
-	cout << a;
+	recipeBook grandmasCookbook("Grandma's Cookbook", "Stored on the top shelf for years!");
+	grandmasCookbook.addRecipe(a);
+
+	cout << grandmasCookbook.getRecipe(0) << "\n\nNow starting test of recipe addition to a book object...\n\n";
+
+	recipeBook book("My Recipe Book", "Contains many recipes.");
+	book = newRecipe(book);
+
+	cout << book.getTitle() << "\n" << book.getDescription() << "\n\n" << book.getRecipe(0);
 
 	return 0;
 }
